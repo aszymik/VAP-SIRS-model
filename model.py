894b6ffc8ad82a11419810deaee7ea3ab6126206
annnie73 <aniaszymik02@gmail.com>
2024-05-30 17:59:20 +0200

    model change

--- model.py
+++ model.py
@@ -8,6 +8,7 @@ def vap_sirs_model(y, _, beta, beta_v, kappa, upsilon, upsilon_r, omega, a, gamm
     I = I_D + I_N
     I_V = I_1 + I_2
 
+
     # Susceptible
     dS_D = - (beta * I + beta * I_V) * S_D + kappa * R_D
     dS_N = - (beta * I + beta * I_V) * S_N - upsilon * S_N + kappa * R_N
@@ -27,6 +28,55 @@ def vap_sirs_model(y, _, beta, beta_v, kappa, upsilon, upsilon_r, omega, a, gamm
     dR_V = gamma * I_V - kappa * R_V - upsilon_r * R_V
     dR_D = gamma * I_D - kappa * R_D
     dR_N = gamma * I_N - kappa * R_N - upsilon * R_N
+
+    return [dS_D, dS_N, dS_1, dS_2, dV, dI_D, dI_N, dI_1, dI_2, dR_V, dR_D, dR_N]
+
+
+def modified_vap_sirs_model(y, _, beta_0, beta_m0, f, f_v, kappa, upsilon, upsilon_r, upsilon_m, upsilon_mr, omega, omega_m, a, gamma):
+    # S1 myślą ze są odporni
+    # S2 wiedzą ze stracili odporność
+
+    # Sn planują się zaszczepić
+    # Sd nie planują się zaszczepić
+    
+    Sd, Sn, Smn, Smd, S1, S2, Sm1, Sm2, V, Id, In, Imn, Imd, I1, I2, Im1, Im2, Rd, Rn, Rmn, Rmd, Rv, Rmv = y
+    I = Id + In 
+    Im = Imn + Imd
+    Iv = I1 + I2
+    Imv = Im1 + Im2
+
+    beta = beta_0 * (1-f)
+    beta_v = beta_0 * (1-f_v)
+    beta_m = beta_m0 * (1-f)
+    beta_mv = beta_m0 * (1-f_v)
+
+    # Susceptible
+    dSd = - (beta * I + beta_m * Im + ) * Sd + kappa * Rd
+    dSn = - (beta * I + beta * Iv) * Sn - upsilon * Sn + kappa * Rn
+    dS1 = upsilon_r * (1 - a) * S2 + upsilon * (1 - a) * Sn - omega * S1 - (beta * I + beta_v * Iv) * S1
+    dS2 = - upsilon_r * S2 + omega * V + omega * S1 - (beta * I + beta_v * Iv) * S2 + kappa * Rv
+
+
+    # More susceptible
+    dSmd = - (beta * I + beta * Iv) * Smd + kappa * Rmd
+    dSmn = - (beta * I + beta * Iv) * Smn - upsilon * Smn + kappa * Rmn
+    dSm1 = upsilon_mr * (1 - a) * Sm2 + upsilon_m * (1 - a) * Smn - omega * Sm1 - (beta * I + beta_v * Iv) * Sm1
+    dSm2 = - upsilon_mr * Sm2 + omega_m * V + omega_m * Sm1 - (beta * I + beta_v * Iv) * Sm2 + kappa * Rmv
+
+    
+    # Vaccinated
+    dV = upsilon * a * Sn + upsilon_r * a * S2 - omega * V + upsilon_r * Rv + upsilon * Rn
+
+    # Infected
+    dI_D = (beta * I + beta * Iv) * Sd - gamma * Id
+    dI_N = (beta * I + beta * Iv) * Sn - gamma * In
+    dI_1 = (beta * I + beta_v * Iv) * S1 - gamma * I1
+    dI_2 = (beta * I + beta_v * Iv) * S2 - gamma * I2
+
+    # Recovered
+    dR_V = gamma * Iv - kappa * Rv - upsilon_r * Rv
+    dR_D = gamma * Id - kappa * Rd
+    dR_N = gamma * In - kappa * Rn - upsilon * Rn
 
     return [dS_D, dS_N, dS_1, dS_2, dV, dI_D, dI_N, dI_1, dI_2, dR_V, dR_D, dR_N]
 
