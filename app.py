0b4f04c372dd245da2b78ec30be24308426e4df3
D4S1 <joanna.bastet@gmail.com>
2024-05-30 21:35:33 +0200

    model update

--- app.py
+++ app.py
@@ -6,41 +6,73 @@ st.title('VAP-SIRS Model Simulation')
 
 with st.sidebar:
     # Suwaki do wyboru parametrów
-    beta = st.slider('Beta', min_value=0.0, max_value=1.0, value=0.23)
-    beta_v = st.slider('Beta V', min_value=0.0, max_value=1.0, value=0.45)
+    beta_0 = st.slider('Beta', min_value=0.0, max_value=5.0, value=1.0)
+    beta_m0 = st.slider('Beta m', min_value=0.0, max_value=5.0, value=2.0)
+
+    f = st.slider('f', min_value=0.0, max_value=1.0, value=0.77)
+    f_v = st.slider('fv', min_value=0.0, max_value=1.0, value=0.55)
+
     kappa = st.slider('Kappa', min_value=0.0, max_value=1.0, value=1/500)
+
     upsilon = st.slider('Upsilon', min_value=0.0, max_value=1.0, value=1/250)
     upsilon_r = st.slider('Upsilon R', min_value=0.0, max_value=1.0, value=1/250)
+    upsilon_m = st.slider('Upsilon m', min_value=0.0, max_value=1.0, value=2/250)
+    upsilon_mr = st.slider('Upsilon m R', min_value=0.0, max_value=1.0, value=2/250)
+    
     omega = st.slider('Omega', min_value=0.0, max_value=1.0, value=1/500)
+    omega_m = st.slider('Omega m', min_value=0.0, max_value=1.0, value=1/400)
+
     a = st.slider('A', min_value=0.0, max_value=1.0, value=0.79)
+    a_m = st.slider('Am', min_value=0.0, max_value=1.0, value=0.60)
+
     gamma = st.slider('Gamma', min_value=0.0, max_value=1.0, value=1/6)
-    d = st.slider('D', min_value=0.0, max_value=1.0, value=0.12)
+    d = st.slider("% of people who won't vaccinate", min_value=0.0, max_value=1.0, value=0.12)
+    m = st.slider('% of more susceptible people', min_value=0.0, max_value=1.0, value=0.12)
     days = st.number_input('Days', min_value=1, max_value=1000, value=750, step=1)
+
+    
 
 # Wartości początkowe
 V = 0
-I_1 = 0
-I_2 = 0
-I_D = d * 10e-6
-I_N = (1-d) * 10e-6
-S = 1 - I_D - I_N
-S_D = d * S
-S_N = (1-d) * S
-S_1 = 0
-S_2 = 0
-R_V = 0
-R_D = 0
-R_N = 0
+
+Id = d * 10e-6 * (1-m)
+In = (1-d) * 10e-6 * (1-m)
+Imd = d * 10e-6 * m
+Imn = (1-d) * 10e-6 * m
+I1 = 0
+I2 = 0
+Im1 = 0
+Im2 = 0
+
+S = 1 - Id - In - Imn - Imd
+Sd = d * S * (1-m)
+Sn = (1-d) * S * (1-m)
+Smn = (1-d) * S * m
+Smd = d * S * m
+S1 = 0
+S2 = 0
+Sm1 = 0
+Sm2 = 0
+
+
+Rd = 0
+Rn = 0
+Rmn = 0
+Rmd = 0
+Rv = 0
+Rmv = 0
+
 
 # Przycisk do uruchomienia symulacji
 if st.button('Run simulation'):
-    initial_conditions = [S_D, S_N, S_1, S_2, V, I_D, I_N, I_1, I_2, R_V, R_D, R_N]
-    result = simulate_vap_sirs_model(initial_conditions, beta, beta_v, kappa, upsilon, upsilon_r, omega, a, gamma, days)
+    initial_conditions = [Sd, Sn, Smn, Smd, S1, S2, Sm1, Sm2, V, Id, In, Imn, Imd, I1, I2, Im1, Im2, Rd, Rn, Rmn, Rmd, Rv, Rmv]
+    result = simulate_vap_sirs_model(initial_conditions, beta_0, beta_m0, f, f_v, kappa, upsilon, upsilon_r, upsilon_m, upsilon_mr, omega, omega_m, a, a_m, gamma, days)
 
-    absolute_values_plot = plot_absolute_values(result)
+    abs_plot1, abs_plot2 = plot_absolute_values(result)
     st.markdown('### Group populations over time')
-    st.pyplot(absolute_values_plot)
+    st.pyplot(abs_plot1)
+    st.pyplot(abs_plot2)
 
-    changes_in_infected_plot = plot_changes_in_infected(result)
-    st.markdown('### Changes in infected populations over time')
-    st.pyplot(changes_in_infected_plot)
+    # changes_in_infected_plot = plot_changes_in_infected(result)
+    # st.markdown('### Changes in infected populations over time')
+    # st.pyplot(changes_in_infected_plot)
--- model.py
+++ model.py
@@ -32,7 +32,7 @@ def vap_sirs_model(y, _, beta, beta_v, kappa, upsilon, upsilon_r, omega, a, gamm
     return [dS_D, dS_N, dS_1, dS_2, dV, dI_D, dI_N, dI_1, dI_2, dR_V, dR_D, dR_N]
 
 
-def modified_vap_sirs_model(y, _, beta_0, beta_m0, f, f_v, kappa, upsilon, upsilon_r, upsilon_m, upsilon_mr, omega, omega_m, a, gamma):
+def modified_vap_sirs_model(y, _, beta_0, beta_m0, f, f_v, kappa, upsilon, upsilon_r, upsilon_m, upsilon_mr, omega, omega_m, a, a_m, gamma):
     # S1 myślą ze są odporni
     # S2 wiedzą ze stracili odporność
 
@@ -40,6 +40,7 @@ def modified_vap_sirs_model(y, _, beta_0, beta_m0, f, f_v, kappa, upsilon, upsil
     # Sd nie planują się zaszczepić
     
     Sd, Sn, Smn, Smd, S1, S2, Sm1, Sm2, V, Id, In, Imn, Imd, I1, I2, Im1, Im2, Rd, Rn, Rmn, Rmd, Rv, Rmv = y
+
     I = Id + In 
     Im = Imn + Imd
     Iv = I1 + I2
@@ -51,66 +52,96 @@ def modified_vap_sirs_model(y, _, beta_0, beta_m0, f, f_v, kappa, upsilon, upsil
     beta_mv = beta_m0 * (1-f_v)
 
     # Susceptible
-    dSd = - (beta * I + beta_m * Im + ) * Sd + kappa * Rd
-    dSn = - (beta * I + beta * Iv) * Sn - upsilon * Sn + kappa * Rn
-    dS1 = upsilon_r * (1 - a) * S2 + upsilon * (1 - a) * Sn - omega * S1 - (beta * I + beta_v * Iv) * S1
-    dS2 = - upsilon_r * S2 + omega * V + omega * S1 - (beta * I + beta_v * Iv) * S2 + kappa * Rv
+    dSd = - beta * (I + Im + Iv + Imv) * Sd + kappa * Rd
+    dSn = - beta * (I + Im + Iv + Imv) * Sn - upsilon * Sn + kappa * Rn
+    dS1 = upsilon_r * (1 - a) * S2 + upsilon * (1 - a) * Sn + upsilon *(1-a)*Rn + upsilon_r*(1-a)*Rv - omega * S1 - (beta * (I + Im) + beta_v * (Iv + Imv)) * S1
+    dS2 = - upsilon_r * S2 + omega * V + omega * S1 - (beta * (I + Im) + beta_v * (Iv + Imv)) * S2 + kappa * Rv
 
 
     # More susceptible
-    dSmd = - (beta * I + beta * Iv) * Smd + kappa * Rmd
-    dSmn = - (beta * I + beta * Iv) * Smn - upsilon * Smn + kappa * Rmn
-    dSm1 = upsilon_mr * (1 - a) * Sm2 + upsilon_m * (1 - a) * Smn - omega * Sm1 - (beta * I + beta_v * Iv) * Sm1
-    dSm2 = - upsilon_mr * Sm2 + omega_m * V + omega_m * Sm1 - (beta * I + beta_v * Iv) * Sm2 + kappa * Rmv
+    dSmd = - beta_m *(I + Im + Iv + Imv) * Smd + kappa * Rmd
+    dSmn = - beta_m *(I + Im + Iv + Imv) * Smn - upsilon_m * Smn + kappa * Rmn
+
+    dSm1 = upsilon_mr * (1 - a_m) * Sm2 + upsilon_m * (1 - a_m) * Smn + upsilon_m *(1-a_m)*Rmn + upsilon_mr*(1-a_m)*Rmv - omega_m * Sm1 - (beta_m * (I + Im) + beta_mv * (Iv+Imv)) * Sm1
+    dSm2 = - upsilon_mr * Sm2 + omega_m * V + omega_m * Sm1 - (beta_m * (I + Im) + beta_mv * (Iv+Imv)) * Sm2 + kappa * Rmv
 
     
     # Vaccinated
-    dV = upsilon * a * Sn + upsilon_r * a * S2 - omega * V + upsilon_r * Rv + upsilon * Rn
+    dV = upsilon * a * Sn + upsilon_m * a_m * Smn + upsilon_r * a * S2 + upsilon_mr * a_m * Sm2 - (omega + omega_m) * V + upsilon_r * a * Rv + upsilon_mr* a_m * Rmv + upsilon * a * Rn + upsilon_m* a_m * Rmn
 
     # Infected
-    dI_D = (beta * I + beta * Iv) * Sd - gamma * Id
-    dI_N = (beta * I + beta * Iv) * Sn - gamma * In
-    dI_1 = (beta * I + beta_v * Iv) * S1 - gamma * I1
-    dI_2 = (beta * I + beta_v * Iv) * S2 - gamma * I2
+    dId = beta * (I + Im + Iv + Imv) * Sd - gamma * Id
+    dIn = beta * (I + Im + Iv + Imv) * Sn - gamma * In
+    dImd = beta_m *(I + Im + Iv + Imv) * Smd - gamma * Imd
+    dImn = beta_m *(I + Im + Iv + Imv) * Smn - gamma * Imn
+
+    dI1 = (beta * (I + Im) + beta_v * (Iv + Imv)) * S1 - gamma * I1
+    dI2 = (beta * (I + Im) + beta_v * (Iv + Imv)) * S2 - gamma * I2
+    dIm1 = (beta_m * (I + Im) + beta_mv * (Iv+Imv)) * Sm1 - gamma * Im1
+    dIm2 = (beta_m * (I + Im) + beta_mv * (Iv+Imv)) * Sm2 - gamma * Im2
 
     # Recovered
-    dR_V = gamma * Iv - kappa * Rv - upsilon_r * Rv
-    dR_D = gamma * Id - kappa * Rd
-    dR_N = gamma * In - kappa * Rn - upsilon * Rn
+    dRv = gamma * Iv - kappa * Rv - upsilon_r * Rv
+    dRmv = gamma * Imv - kappa * Rmv - upsilon_mr * Rmv
 
-    return [dS_D, dS_N, dS_1, dS_2, dV, dI_D, dI_N, dI_1, dI_2, dR_V, dR_D, dR_N]
+    dRd = gamma * Id - kappa * Rd
+    dRn = gamma * In - kappa * Rn - upsilon * Rn
+    dRmd = gamma * Imd - kappa * Rmd
+    dRmn = gamma * Imn - kappa * Rmn - upsilon_m * Rmn
+
+    return [dSd, dSn, dSmn, dSmd, dS1, dS2, dSm1, dSm2, dV, dId, dIn, dImn, dImd, dI1, dI2, dIm1, dIm2, dRd, dRn, dRmn, dRmd, dRv, dRmv]
 
 
-def simulate_vap_sirs_model(initial_conditions, beta, beta_v, kappa, upsilon, upsilon_r, omega, a, gamma, days):
+def simulate_vap_sirs_model(initial_conditions, beta_0, beta_m0, f, f_v, kappa, upsilon, upsilon_r, upsilon_m, upsilon_mr, omega, omega_m, a, a_m, gamma, days):
     t = np.linspace(0, days, days)  # time points
     y0 = initial_conditions
-    return odeint(vap_sirs_model, y0, t, args=(beta, beta_v, kappa, upsilon, upsilon_r, omega, a, gamma))
+    return odeint(modified_vap_sirs_model, y0, t, args=(beta_0, beta_m0, f, f_v, kappa, upsilon, upsilon_r, upsilon_m, upsilon_mr, omega, omega_m, a, a_m, gamma))
 
 
 def plot_absolute_values(result):
     days = len(result)
     t = np.linspace(0, days, days)
-    S_D, S_N, S_1, S_2, V, I_D, I_N, I_1, I_2, R_V, R_D, R_N = [result[:, i] for i in range(len(result[0]))]
-    
-    fig, axes = plt.subplots()
-    plt.plot(t, S_D, label='S_D')
-    plt.plot(t, S_N, label='S_N')
-    plt.plot(t, S_1, label='S_1')
-    plt.plot(t, S_2, label='S_2')
+    Sd, Sn, Smn, Smd, S1, S2, Sm1, Sm2, V, Id, In, Imn, Imd, I1, I2, Im1, Im2, Rd, Rn, Rmn, Rmd, Rv, Rmv = [result[:, i] for i in range(len(result[0]))]
+
+    # Variables without 'm'
+    fig1, axes1 = plt.subplots()
+    plt.plot(t, Sd, label='Sd')
+    plt.plot(t, Sn, label='Sn')
+    plt.plot(t, S1, label='S1')
+    plt.plot(t, S2, label='S2')
     plt.plot(t, V, label='V')
-    plt.plot(t, I_D, label='I_D')
-    plt.plot(t, I_N, label='I_N')
-    plt.plot(t, I_1, label='I_1')
-    plt.plot(t, I_2, label='I_2')
-    plt.plot(t, R_V, label='R_V')
-    plt.plot(t, R_D, label='R_D')
-    plt.plot(t, R_N, label='R_N')
+    plt.plot(t, Id, label='Id')
+    plt.plot(t, In, label='In')
+    plt.plot(t, I1, label='I1')
+    plt.plot(t, I2, label='I2')
+    plt.plot(t, Rd, label='Rd')
+    plt.plot(t, Rn, label='Rn')
     plt.xlabel('Time (days)')
     plt.ylabel('Population')
-    plt.title('VAP-SIRS Model Simulation')
+    plt.title('VAP-SIRS Model Simulation for normal people')
     plt.legend()
     plt.grid(True)
-    return fig
+
+    # Variables with 'm'
+    fig2, axes2 = plt.subplots()
+    plt.plot(t, Smn, label='Smn')
+    plt.plot(t, Smd, label='Smd')
+    plt.plot(t, Sm1, label='Sm1')
+    plt.plot(t, Sm2, label='Sm2')
+    plt.plot(t, Imn, label='Imn')
+    plt.plot(t, Imd, label='Imd')
+    plt.plot(t, Im1, label='Im1')
+    plt.plot(t, Im2, label='Im2')
+    plt.plot(t, Rmn, label='Rmn')
+    plt.plot(t, Rmd, label='Rmd')
+    plt.xlabel('Time (days)')
+    plt.ylabel('Population')
+    plt.title('VAP-SIRS Model Simulation for more susceptible people')
+    plt.legend()
+    plt.grid(True)
+
+    return fig1, fig2
+
 
 def plot_changes_in_infected(result):
     days = len(result)
