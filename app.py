5116dd0f11efb08c950101f86a068b4f7680a04c
annnie73 <aniaszymik02@gmail.com>
2024-06-04 11:34:19 +0200

    add differences plot and change plots to plotly

--- app.py
+++ app.py
@@ -6,47 +6,50 @@ st.title('VAP-SIRS Model Simulation')
 
 with st.sidebar:
     # Suwaki do wyboru parametrów
-    beta_0 = st.slider('Beta', min_value=0.0, max_value=5.0, value=1.0)
-    beta_m0 = st.slider('Beta m', min_value=0.0, max_value=5.0, value=2.0)
+    beta_0 = st.slider('Beta', min_value=0.0, max_value=1.0, value=0.9)
+    beta_m0 = st.slider('Beta m', min_value=0.0, max_value=1.0, value=1.0)
 
     f = st.slider('f', min_value=0.0, max_value=1.0, value=0.77)
     f_v = st.slider('fv', min_value=0.0, max_value=1.0, value=0.55)
-
-    kappa = st.slider('Kappa', min_value=0.0, max_value=1.0, value=1.0)
 
     upsilon = st.slider('Upsilon', min_value=0.0, max_value=1.0, value=0.04)
     upsilon_r = st.slider('Upsilon R', min_value=0.0, max_value=1.0, value=0.03)
     upsilon_m = st.slider('Upsilon m', min_value=0.0, max_value=1.0, value=0.06)
     upsilon_mr = st.slider('Upsilon m R', min_value=0.0, max_value=1.0, value=0.06)
     
-    omega = st.slider('Omega', min_value=0.0, max_value=0.01, value=1/500)
-    omega_m = st.slider('Omega m', min_value=0.0, max_value=0.01, value=1/600)
+    # omega = st.slider('Omega', min_value=0.0, max_value=0.01, value=1/500)
+    # omega_m = st.slider('Omega m', min_value=0.0, max_value=0.01, value=1/600)
+
+    kappa = 1/st.slider('Period of losing natural immunity', min_value=30, max_value=700, value=500)
+    omega = 1/st.slider('Period of losing vaccine-induced immunity for normal people', min_value=30, max_value=700, value=500)
+    omega_m = 1/st.slider('Period of losing vaccine-induced immunity for more susceptible people', min_value=30, max_value=700, value=600)
 
     a = st.slider('A', min_value=0.0, max_value=1.0, value=0.79)
     a_m = st.slider('Am', min_value=0.0, max_value=1.0, value=0.60)
 
     gamma = st.slider('Gamma', min_value=0.0, max_value=1.0, value=1/6)
     d = st.slider("% of people who won't vaccinate", min_value=0.0, max_value=1.0, value=0.12)
-    m = st.slider('% of more susceptible people', min_value=0.0, max_value=1.0, value=0.10)
+    m = st.slider('% of more susceptible people', min_value=0.0, max_value=1.0, value=0.05)
     days = st.number_input('Days', min_value=1, max_value=2000, value=750, step=1)
 
     
 
 # Wartości początkowe
-N = 100
+N = 10000
 V = 0
 Vm = 0
+I_percentage = 10e-6
 
-Id = d * 10e-6 * (1-m) * N
-In = (1-d) * 10e-6 * (1-m) * N
-Imd = d * 10e-6 * m * N
-Imn = (1-d) * 10e-6 * m * N
+Id = d * (1-m) * I_percentage * N
+In = (1-d) * (1-m) * I_percentage * N
+Imd = d * m * I_percentage * N
+Imn = (1-d) * m * I_percentage * N
 I1 = 0
 I2 = 0
 Im1 = 0
 Im2 = 0
 
-S = N * 1 - Id - In - Imn - Imd
+S = N - Id - In - Imn - Imd
 Sd = d * S * (1-m)
 Sn = (1-d) * S * (1-m)
 Smn = (1-d) * S * m
@@ -70,14 +73,15 @@ Rmv = 0
 # Przycisk do uruchomienia symulacji
 if st.button('Run simulation'):
     initial_conditions = [Sd, Sn, Smn, Smd, S1, S2, Sm1, Sm2, V, Vm, Id, In, Imn, Imd, I1, I2, Im1, Im2, Rd, Rn, Rmn, Rmd, Rv, Rmv]
+    
     # print(f'{sum(initial_conditions)=}')
     result = simulate_vap_sirs_model(initial_conditions, beta_0, beta_m0, f, f_v, kappa, upsilon, upsilon_r, upsilon_m, upsilon_mr, omega, omega_m, a, a_m, gamma, days)
 
     abs_plot1, abs_plot2 = plot_absolute_values(result)
     st.markdown('### Group populations over time')
-    st.pyplot(abs_plot1)
-    st.pyplot(abs_plot2)
+    st.plotly_chart(abs_plot1)
+    st.plotly_chart(abs_plot2)
 
-    # changes_in_infected_plot = plot_changes_in_infected(result)
-    # st.markdown('### Changes in infected populations over time')
-    # st.pyplot(changes_in_infected_plot)
+    inf_change_plot = plot_changes_in_infected(result)
+    st.markdown('### Changes in infected populations over time')
+    st.plotly_chart(inf_change_plot)
--- model.py
+++ model.py
@@ -1,35 +1,6 @@
 from scipy.integrate import odeint
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 import numpy as np
-
-
-def vap_sirs_model(y, _, beta, beta_v, kappa, upsilon, upsilon_r, omega, a, gamma):
-    S_D, S_N, S_1, S_2, V, I_D, I_N, I_1, I_2, R_V, R_D, R_N = y
-    I = I_D + I_N
-    I_V = I_1 + I_2
-
-
-    # Susceptible
-    dS_D = - (beta * I + beta * I_V) * S_D + kappa * R_D
-    dS_N = - (beta * I + beta * I_V) * S_N - upsilon * S_N + kappa * R_N
-    dS_1 = upsilon_r * (1 - a) * S_2 + upsilon * (1 - a) * S_N - omega * S_1 - (beta * I + beta_v * I_V) * S_1
-    dS_2 = - upsilon_r * S_2 + omega * V + omega * S_1 - (beta * I + beta_v * I_V) * S_2 + kappa * R_V
-
-    # Vaccinated
-    dV = upsilon * a * S_N + upsilon_r * a * S_2 - omega * V + upsilon_r * R_V + upsilon * R_N
-
-    # Infected
-    dI_D = (beta * I + beta * I_V) * S_D - gamma * I_D
-    dI_N = (beta * I + beta * I_V) * S_N - gamma * I_N
-    dI_1 = (beta * I + beta_v * I_V) * S_1 - gamma * I_1
-    dI_2 = (beta * I + beta_v * I_V) * S_2 - gamma * I_2
-
-    # Recovered
-    dR_V = gamma * I_V - kappa * R_V - upsilon_r * R_V
-    dR_D = gamma * I_D - kappa * R_D
-    dR_N = gamma * I_N - kappa * R_N - upsilon * R_N
-
-    return [dS_D, dS_N, dS_1, dS_2, dV, dI_D, dI_N, dI_1, dI_2, dR_V, dR_D, dR_N]
 
 
 def modified_vap_sirs_model(y, _, beta_0, beta_m0, f, f_v, kappa, upsilon, upsilon_r, upsilon_m, upsilon_mr, omega, omega_m, a, a_m, gamma):
@@ -91,6 +62,8 @@ def modified_vap_sirs_model(y, _, beta_0, beta_m0, f, f_v, kappa, upsilon, upsil
     dRmn = gamma * Imn - kappa * Rmn - upsilon_m * Rmn
 
     # print(f'{S1=}\t{S2=}')
+    # print(sum([dSd, dSn, dSmn, dSmd, dS1, dS2, dSm1, dSm2, dV, dVm, dId, dIn, dImn, dImd, dI1, dI2, dIm1, dIm2, dRd, dRn, dRmn, dRmd, dRv, dRmv]))
+
     return [dSd, dSn, dSmn, dSmd, dS1, dS2, dSm1, dSm2, dV, dVm, dId, dIn, dImn, dImd, dI1, dI2, dIm1, dIm2, dRd, dRn, dRmn, dRmd, dRv, dRmv]
 
 
@@ -106,42 +79,34 @@ def plot_absolute_values(result):
     Sd, Sn, Smn, Smd, S1, S2, Sm1, Sm2, V, Vm, Id, In, Imn, Imd, I1, I2, Im1, Im2, Rd, Rn, Rmn, Rmd, Rv, Rmv = [result[:, i] for i in range(len(result[0]))]
 
     # Variables without 'm'
-    fig1, axes1 = plt.subplots()
-    plt.plot(t, Sd, label='Sd')
-    plt.plot(t, Sn, label='Sn')
-    plt.plot(t, S1, label='S1')
-    plt.plot(t, S2, label='S2')
-    # plt.plot(t, V, label='V')
-    # plt.plot(t, Id, label='Id')
-    # plt.plot(t, In, label='In')
-    # plt.plot(t, I1, label='I1')
-    # plt.plot(t, I2, label='I2')
-    # plt.plot(t, Rd, label='Rd')
-    # plt.plot(t, Rn, label='Rn')
-    plt.xlabel('Time (days)')
-    plt.ylabel('Population')
-    plt.title('VAP-SIRS Model Simulation for normal people')
-    plt.legend()
-    plt.grid(True)
+    fig1 = go.Figure()
+    fig1.add_trace(go.Scatter(x=t, y=Sd, mode='lines', name='Sd'))
+    fig1.add_trace(go.Scatter(x=t, y=Sn, mode='lines', name='Sn'))
+    fig1.add_trace(go.Scatter(x=t, y=S1, mode='lines', name='S1'))
+    fig1.add_trace(go.Scatter(x=t, y=S2, mode='lines', name='S2'))
+    fig1.add_trace(go.Scatter(x=t, y=V, mode='lines', name='V'))
+    fig1.add_trace(go.Scatter(x=t, y=Id, mode='lines', name='Id'))
+    fig1.add_trace(go.Scatter(x=t, y=In, mode='lines', name='In'))
+    fig1.add_trace(go.Scatter(x=t, y=I1, mode='lines', name='I1'))
+    fig1.add_trace(go.Scatter(x=t, y=I2, mode='lines', name='I2'))
+    fig1.add_trace(go.Scatter(x=t, y=Rd, mode='lines', name='Rd'))
+    fig1.add_trace(go.Scatter(x=t, y=Rn, mode='lines', name='Rn'))
+    fig1.update_layout(title='VAP-SIRS Model Simulation for normal people', xaxis_title='Time (days)', yaxis_title='Population')
 
     # Variables with 'm'
-    fig2, axes2 = plt.subplots()
-    plt.plot(t, Smn, label='Smn')
-    plt.plot(t, Smd, label='Smd')
-    plt.plot(t, Sm1, label='Sm1')
-    plt.plot(t, Sm2, label='Sm2')
-    plt.plot(t, Vm, label='Vm')
-    plt.plot(t, Imn, label='Imn')
-    plt.plot(t, Imd, label='Imd')
-    plt.plot(t, Im1, label='Im1')
-    plt.plot(t, Im2, label='Im2')
-    plt.plot(t, Rmn, label='Rmn')
-    plt.plot(t, Rmd, label='Rmd')
-    plt.xlabel('Time (days)')
-    plt.ylabel('Population')
-    plt.title('VAP-SIRS Model Simulation for more susceptible people')
-    plt.legend()
-    plt.grid(True)
+    fig2 = go.Figure()
+    fig2.add_trace(go.Scatter(x=t, y=Smn, mode='lines', name='Smn'))
+    fig2.add_trace(go.Scatter(x=t, y=Smd, mode='lines', name='Smd'))
+    fig2.add_trace(go.Scatter(x=t, y=Sm1, mode='lines', name='Sm1'))
+    fig2.add_trace(go.Scatter(x=t, y=Sm2, mode='lines', name='Sm2'))
+    fig2.add_trace(go.Scatter(x=t, y=Vm, mode='lines', name='Vm'))
+    fig2.add_trace(go.Scatter(x=t, y=Imn, mode='lines', name='Imn'))
+    fig2.add_trace(go.Scatter(x=t, y=Imd, mode='lines', name='Imd'))
+    fig2.add_trace(go.Scatter(x=t, y=Im1, mode='lines', name='Im1'))
+    fig2.add_trace(go.Scatter(x=t, y=Im2, mode='lines', name='Im2'))
+    fig2.add_trace(go.Scatter(x=t, y=Rmn, mode='lines', name='Rmn'))
+    fig2.add_trace(go.Scatter(x=t, y=Rmd, mode='lines', name='Rmd'))
+    fig2.update_layout(title='VAP-SIRS Model Simulation for more susceptible people', xaxis_title='Time (days)', yaxis_title='Population')
 
     return fig1, fig2
 
@@ -151,20 +116,18 @@ def plot_changes_in_infected(result):
     t = np.linspace(0, days, days)
     diff_result = np.diff(result, axis=0)  # calculate the differences
 
-    I_D, I_N, I_1, I_2 = [diff_result[:, i] for i in range(5, 9)]
-    I_sigma = I_N + I_D + I_1 + I_2
-    I = I_N + I_D
+    Id, In, Imn, Imd, I1, I2, Im1, Im2 = [diff_result[:, i] for i in range(10, 18)]
+    susceptible_not_vaccinated = Imn + Imd
+    susceptible_vaccinated = Im1 + Im2
+    normal_not_vaccinated = In + Id
+    normal_vaccinated = I1 + I2
 
-    fig, axes = plt.subplots()
-    plt.plot(t[:-1], I, label='I')
-    plt.plot(t[:-1], I_sigma, label='I_sigma')
-    plt.plot(t[:-1], I_1, label='I_1')
-    plt.plot(t[:-1], I_2, label='I_2')
-    plt.xlabel('Time (days)')
-    plt.ylabel('Change in population')
-    plt.title('VAP-SIRS Model Simulation (Differences)')
-    plt.legend()
-    plt.grid(True)
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=t[:-1], y=susceptible_not_vaccinated, mode='lines', name='Susceptible not vaccinated'))
+    fig.add_trace(go.Scatter(x=t[:-1], y=susceptible_vaccinated, mode='lines', name='Susceptible vaccinated'))
+    fig.add_trace(go.Scatter(x=t[:-1], y=normal_not_vaccinated, mode='lines', name='Normal not vaccinated'))
+    fig.add_trace(go.Scatter(x=t[:-1], y=normal_vaccinated, mode='lines', name='Normal vaccinated'))
+    fig.update_layout(title='VAP-SIRS Model Simulation (Differences)', xaxis_title='Time (days)', yaxis_title='Change in infected population')
+
     return fig
-
 
